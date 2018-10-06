1164df80c1373dc259f947ae8e343106779fa17d
chenli <chenli@haituncun.com>
2018-10-06 11:48:29 +0800

    text

--- wechat/message/message.go
+++ wechat/message/message.go
@@ -5,24 +5,15 @@ import (
 	"gopkg.in/chanxuehong/wechat.v2/mp/message/callback/response"
 )
 
-func Text(ctx *core.Context, content string) response.Text {
-	text := response.Text{
-		MsgHeader: core.MsgHeader{
-			FromUserName: ctx.MixedMsg.MsgHeader.ToUserName,
-			ToUserName:   ctx.MixedMsg.MsgHeader.FromUserName,
-			CreateTime:   ctx.MixedMsg.MsgHeader.CreateTime,
-			MsgType:      response.MsgTypeText,
-		},
-		Content: content,
-	}
+func Text(ctx *core.Context, content string) *response.Text {
+	text := response.NewText(ctx.MixedMsg.MsgHeader.FromUserName, ctx.MixedMsg.MsgHeader.ToUserName,
+		ctx.MixedMsg.MsgHeader.CreateTime, content)
 	return text
 }
 
 func Image(ctx *core.Context, mediaId string) *response.Image {
-
 	image := response.NewImage(ctx.MixedMsg.MsgHeader.FromUserName, ctx.MixedMsg.MsgHeader.ToUserName,
 		ctx.MixedMsg.MsgHeader.CreateTime, mediaId)
 	image.Image.MediaId = mediaId
-
 	return image
 }
