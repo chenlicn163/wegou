c9e8e62d5a81ca1d411e750fb346ef96283782e4
chenlicn163 <chenlicn163@163.com>
2018-10-17 22:23:53 +0800

    demo data

--- doc/demo.sql
+++ doc/demo.sql
@@ -1,2 +1,2 @@
 INSERT INTO `account` VALUES (1, 'wear', '', 0, 0, 0, 1);
-INSERT INTO `wechat` VALUES (1, '时尚穿衣', 'clothes', 'wx5d27d17dffa7b688', '53c83de26e370fcd9d592e0b6666e63b', '', 'gh_e8cf74e4d7bb', 'opentpcmsWechat', 0, 0, 5, 1, 1, '127.0.0.1', 'wechat', 3306, 'root', '', 0, 1);
+INSERT INTO `wechat` VALUES (1, '时尚穿衣', 'clothes', 'wx5d27d17dffa7b688', '53c83de26e370fcd9d592e0b6666e63b', '', 'gh_e8cf74e4d7bb', 'opentpcmsWechat', 0, 0, 5, 1, 1, '127.0.0.1', 'wechat', '3306', 'root', '', 0, 1);
