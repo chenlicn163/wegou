068d952a600c76be517b39acc16657e0944f3577
chenli <chenli@haituncun.com>
2018-10-20 00:05:36 +0800

    db

--- utils/database.go
+++ utils/database.go
@@ -12,7 +12,7 @@ type Database interface {
 	Open() *gorm.DB
 }
 
-func GetDb(web string) (dbMysql *database.Mysql) {
+func GetDb(web string) (db *database.Mysql) {
 	toolsConfig := config.GetToolsConfig()
 
 	conf := types.Db{}
@@ -24,13 +24,13 @@ func GetDb(web string) (dbMysql *database.Mysql) {
 	}
 	switch toolsConfig.Database {
 	case "mysql":
-		dbMysql = &database.Mysql{}
-		dbMysql.DbUser = conf.DbUser
-		dbMysql.DbPassword = conf.DbPassword
-		dbMysql.DbHost = conf.DbHost
-		dbMysql.DbPort = conf.DbPort
-		dbMysql.DbName = conf.DbName
+		db = &database.Mysql{}
+		db.DbUser = conf.DbUser
+		db.DbPassword = conf.DbPassword
+		db.DbHost = conf.DbHost
+		db.DbPort = conf.DbPort
+		db.DbName = conf.DbName
 	}
 
-	return dbMysql
+	return db
 }
